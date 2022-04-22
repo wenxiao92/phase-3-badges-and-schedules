42685e6d6faabeb3ca5d4e746ea1c5e091e625ea
Wen Xiao <wennstyle@gmail.com>
2022-04-22 16:45:13 -0400

    commit tree e5bb86252b4d4348ede99afc59cf3d470bc6075e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+    speakers.map do |speaker|
+        badge_maker(speaker)
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_num|
+        "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(speakers)
+    speakers.each do |speaker|
+        puts badge_maker(speaker)
+    end
+
+    assign_rooms(speakers).each do |assignment|
+        puts assignment
+    end
+end
